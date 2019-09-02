85c50fb0c5a396f9652d6cf74d231efff8ed9076
sample <dorothy_007@naver.com>
2019-09-03 02:09:43 +0900

    #3.7 Passport JWT part.4

--- src/passport.js
+++ src/passport.js
@@ -20,4 +20,13 @@ const verifyUser = async (payload, done) => {
     }
 };
 
+export const authenticateJwt = (req, res, next) =>
+    passport.authenticate("jwt", { sessions: false }, (error, user) => {
+        if (user) {
+            req.user = user;
+        }
+        next();
+    })(req, res, next);
+
 passport.use(new Strategy(jwtOptions, verifyUser));
+passport.initialize();
--- src/server.js
+++ src/server.js
@@ -3,12 +3,17 @@ import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
 import "./passport";   
+import { authenticateJwt } from "./passport";
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema });
+const server = new GraphQLServer({   
+    schema,
+    context: ({ request }) => ({ request })
+});
 
 server.express.use(logger("dev"));
+server.express.use(authenticateJwt);
 
 server.start({ port: PORT }, () =>
     console.log(`Server running on  http://localhost:${PORT}`)
