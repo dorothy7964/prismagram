82e0881fbe94f84563a61a924204236c1310e7c9
sample <dorothy_007@naver.com>
2019-09-04 20:26:13 +0900

    #3.11 searchUser Condition

--- src/api/User/searchUser/searchUser.js
+++ src/api/User/searchUser/searchUser.js
@@ -2,14 +2,23 @@ import { prisma } from '../../../../generated/prisma-client';
 
 export default {
     Query: {
-        searchUser: async(_, args) => prisma.users({
-            where: {
-                OR: [
-                    { userName_contains: args.term },
-                    { firstName_contains: args.term },
-                    { lastName_contains: args.term }
-                ]
+        searchUser: async(_, args) => {
+            const { term } = args;
+
+            if(term.length > 0){
+                const users = await prisma.users({
+                    where: {
+                        OR: [
+                            { userName_contains: args.term },
+                            { firstName_contains: args.term },
+                            { lastName_contains: args.term }
+                        ]
+                    }
+                });
+                return users;
+            }else {
+                throw Error("Please enter a character to Serch");
             }
-        })
+        }
     }
 };
