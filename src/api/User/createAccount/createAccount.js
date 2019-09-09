54101d4b96a23dc26fbda1e1538aefca975ce9b2
sample <dorothy_007@naver.com>
2019-09-09 19:33:32 +0900

    #3.23 Computed Fields.Message

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,13 +3,14 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
     Mutation: {
         createAccount: async(_, args) => {
-            const { userName, email, firstName = "", lastName = "", bio = "" } = args;
+            const { userName, email, firstName = "", lastName = "", bio = "", avatar = "" } = args;
             const user = await prisma.createUser({
                 userName, 
                 email, 
                 firstName, 
                 lastName, 
-                bio
+                bio,
+                avatar
             });
             return user;
         }
