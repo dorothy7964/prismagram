4233c97350f7809c911dd0b631b9c761bc4fb380
sample <dorothy_007@naver.com>
2019-09-28 22:13:08 +0900

    Avatar default

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,7 +3,7 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
     Mutation: {
         createAccount: async(_, args) => {
-            const { userName, email, firstName = "", lastName = "", bio = "", avatar = "" } = args;
+            const { userName, email, firstName = "", lastName = "", bio = "", avatar } = args;
             const exists = await prisma.$exists.user({
                 OR: [
                     {userName},
