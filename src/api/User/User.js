92b927b3a103351d7ce8bfdff38efe005360b927
sample <dorothy_007@naver.com>
2020-01-06 15:07:44 +0900

    #4.0 allUsers Resolver

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -28,20 +28,19 @@ export default {
             const { user } = request;
             const { id: parentId } = parent;
             try {
-            return prisma.$exists.user({
-                AND: [
-                {
-                    id: user.id
-                },
-                {
-                    following_some: {
-                    id: parentId
-                    }
-                }
-                ]
-            });
+                return prisma.$exists.user({
+                    AND: [{
+                    
+                        id: user.id
+                    },{
+                        following_some: {
+                            id: parentId
+                        }
+                    
+                    }]
+                });
             } catch {
-            return false;
+                return false;
             }
         },
         isSelf: (parent, _, { request }) => {
