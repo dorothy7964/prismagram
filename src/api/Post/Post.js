ea07a0b71376483dd1cf2745305bcee80c265281
sample <dorothy_007@naver.com>
2020-01-21 19:43:22 +0900

    # comments - orderBy : createdAt_DESC

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -3,7 +3,7 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
     Post: {
         files: ({ id }) => prisma.post({ id }).files(),
-        comments: ({ id }) => prisma.post({ id }).comments(),
+        comments: ({ id }) => prisma.post({ id }).comments({orderBy:"createdAt_DESC"}),
         user: ({ id }) => prisma.post({ id }).user(),
         likes: ({ id }) => prisma.post({ id }).likes(),
         isLiked: (parent, _, { request }) => {
