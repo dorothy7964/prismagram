f607221dcb556acdd20882c98a6e280c76bf69c7
sample <dorothy_007@naver.com>
2020-01-22 19:49:32 +0900

    #5.0 PaginateFeed Resolver

---
+++ src/api/Post/paginateFeed/paginateFeed.js
@@ -0,0 +1,24 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Query: {
+        paginateFeed: async (_, args, { request, isAuthenticated }) => {
+            console.log(args.pageNumber);
+            isAuthenticated(request);
+            const { user } = request;
+            const following = await prisma.user({ id: user.id }).following();
+
+            return prisma.posts({
+                first: args.items,
+                skip: args.pageNumber,
+                where: {
+                    user: {
+                        id_in: [...following.map(user => user.id),user.id]
+                    }
+                },
+                orderBy: "createdAt_DESC"
+
+            })
+        }
+    }
+}
