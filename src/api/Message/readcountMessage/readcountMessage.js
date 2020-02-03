952b7e5b3b00223562955f15ee5927c633e962f3
sample <dorothy_007@naver.com>
2020-02-04 02:08:23 +0900

    #6.1 chat - readcountMessage Resolver

---
+++ src/api/Message/readcountMessage/readcountMessage.js
@@ -0,0 +1,44 @@
+import {isAuthenticated} from "../../../middlewares"
+import { prisma } from "../../../../generated/prisma-client";
+
+export default{
+    Mutation:{
+        readcountMessage: async (_, args, { request }) => {
+            isAuthenticated(request);
+            const { user } = request;
+            const { roomId } = args;
+            const filterOptions = {
+                AND: [{
+                        to: {
+                            id: user.id
+                        }
+                    },{
+                        room: {
+                            id: roomId
+                        }
+                    },{
+                        readMessage:false
+                }]
+            };
+
+            try {
+                console.log("readMessage");
+                const result = await prisma.updateManyMessages({
+                    where: filterOptions,
+                    data: { readMessage: true }
+                });
+                
+                if(result){
+                    return true;
+                }
+                return false;
+
+            } catch (e) {
+                console.log(e);
+                console.log("readcountMessage");
+            }
+
+            return false;
+        }
+    }
+}
