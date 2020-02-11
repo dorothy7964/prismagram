952fdea87240e3d22437dad089e7ada67b457737
sample <dorothy_007@naver.com>
2020-02-11 14:38:44 +0900

    #8.1 readCheck Resolver

---
+++ src/api/Like/readLike/readCheck.js
@@ -0,0 +1,26 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default{
+    Mutation:{
+        readLike: async (_, args) => {
+            const { likeId } = args;
+
+            try {
+                const result = await prisma.updateLike({
+                    where: { id: likeId },
+                    data: { readCheck: true }
+                });
+                
+                if(result){
+                    return true;
+                }
+                return false;
+
+            } catch (e) {
+                console.log(e);
+            }
+
+            return false;
+        }
+    }
+}
